c19b745a7dfa2350673afed1c086c5cf4c075dfd
Matthew David Elgert <mdelgert@yahoo.com>
2024-10-30 17:48:03 -0400

    Add base PassTxt.HybridApp

---
+++ PassTxt.HybridApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PassTxt.HybridApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
